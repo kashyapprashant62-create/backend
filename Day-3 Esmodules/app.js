70896413fcb190fbceb6576c478deba8ea9a6581
prashant kashyap <kashyapprashant62@gmail.com>
2026-09-17 16:14:44 +0530

    first commit

---
+++ Day-3 Esmodules/app.js
@@ -0,0 +1,10 @@
+//  const sum = (a,b) => a+b
+
+
+//   export default sum ;
+
+
+
+ export const sum = (a,b) => a+b;
+ export const pow = (a,b) => a+b;
+ export const greet = (user) => `hello ${user}`
---
+++ Day-3 Esmodules/script.js
@@ -0,0 +1,13 @@
+// import sum from "./app.js"
+
+//  console.log(sum(10,20));
+
+
+
+ import {pow,greet,sum} from "./app.js"
+
+ console.log(pow(2,2));
+ console.log(sum(4,6));
+ console.log(greet("raj"));
+ 
+ 
