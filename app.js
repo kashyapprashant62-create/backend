a9e3720ce956afcf0387195f0ad21a890822f4fe
prashant kashyap <kashyapprashant62@gmail.com>
2026-09-21 13:55:14 +0530

    uptade

--- app.js
+++ app.js
@@ -4,17 +4,17 @@
  * Linux Os = F:/Premium Batch/M-8 Premium Backend 15 Sep/NODE
  */
 
-const path = require("path");
+// const path = require("path");
 
 
-  path.resolve(): 
-    const output = path.resolve("chombu");
-    console.log("output:",output);
+//   path.resolve(): 
+//     const output = path.resolve("chombu");
+//     console.log("output:",output);
  
 
 
-  path.join()
-  path.join(arg1, arg2, arg3)
+//   path.join()
+//   path.join(arg1, arg2, arg3)
 
-const output = path.join(__dirname, "logs", "user.txt");
-console.log("output:", output);
+// const output = path.join(__dirname, "logs", "user.txt");
+// console.log("output:", output);
